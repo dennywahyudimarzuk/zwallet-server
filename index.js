9c29a49029b6ced6bb09e49d4e45bbff8b512e2f
rizkyagung18 <55870446+rizkyagung18@users.noreply.github.com>
2020-10-15 00:33:23 +0700

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const morgan = require('morgan')
 const routeNav = require('./src/')
 require('dotenv').config()
 
@@ -10,7 +9,6 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
-app.use(morgan('dev'))
 
 app.use('/api/v1', routeNav)
 
